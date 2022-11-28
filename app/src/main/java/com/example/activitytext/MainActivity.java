f88b227b794806a2f276336f10ae646ca2826066
yn030727 <2601098388@qq.com>
2022-11-28 21:57:47 +0800

    2.Activity的启动模式

--- app/src/main/java/com/example/activitytext/MainActivity.java
+++ app/src/main/java/com/example/activitytext/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.activitytext;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
@@ -15,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView textView = findViewById(R.id.textView);
         textView.setText("这是一个测试案例");
+
+        //通过在Intent中设置标志位来为Activity指定启动模式
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this,SecondActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
     }
 
     //在TextView的源码当中，这两个方法会保存文本选中状态和文本内容
